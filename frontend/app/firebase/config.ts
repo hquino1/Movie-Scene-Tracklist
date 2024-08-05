bb15ffb46329757abf9a19fea70fe02fd7c0bb1f
Hector Quinones <quinonhector1@gmail.com>
2024-08-05 12:56:10 -0500

    implementing auth with firebase

--- frontend/app/firebase/config.ts
+++ frontend/app/firebase/config.ts
@@ -1,5 +1,4 @@
 import { initializeApp, getApps, getApp} from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
@@ -13,8 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 
--- frontend/app/sign-up/page.tsx
+++ frontend/app/sign-up/page.tsx
@@ -3,21 +3,41 @@ import React, { useState } from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import './pages.modules.css';
 import logoImg from '../../public/assets/videocam.png';
+import { useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import {auth} from '@/app/firebase/config';
+import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth/web-extension';
 
 const page = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
-  const handleSubmit = (e: any) => {
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    // Handle form submission logic
-    console.log('Username:', username);
-    console.log('Password:', password);
+
+    try {
+      const email = username;
+      const res = await createUserWithEmailAndPassword(email, password);
+      console.log({res});
+      setUsername('');
+      setPassword('');
+    } catch(error) {
+      console.error("Error trying to submit username and password", error)
+      console.error(JSON.stringify(error, null, 2));
+    }
   };
 
-  const handleGoogleSuccess = (credentialResponse: any) => {
+  const handleGoogleSuccess = async (credentialResponse: any) => {
     console.log('Google Success:', credentialResponse);
     // Handle Google login success logic
+    const credential = GoogleAuthProvider.credential(credentialResponse.credential);
+    try{
+      const result = await signInWithCredential(auth, credential);
+      console.log(result.user);
+    } catch (error) {
+      console.error("Error signing in with google crecentials");
+    }
   };
 
   const handleGoogleFailure = (error: any) => {
@@ -31,7 +51,7 @@ const page = () => {
         <img src={logoImg.src} className='logoImg'></img>
         <h1 className='logoText'>Movie Sound Scene Database</h1>
       </span>
-      <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}>
         <div className="signup-container">
           <h2>Sign Up</h2>
           <form onSubmit={handleSubmit}>
