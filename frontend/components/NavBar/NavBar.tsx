d999ee9126570f1563e79221740f52087c08b550
Hector Quinones <quinonhector1@gmail.com>
2024-08-07 17:36:48 -0500

    implemented search bar

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import styles from './NavBar.module.css';
 import logoImg from '../../public/assets/Screenshot 2024-08-06 164631.png';
+import searchIcon from '../../public/assets/graySearch.png';
 
 const NavBar = () => {
     return (
@@ -8,7 +9,12 @@ const NavBar = () => {
             <div className={styles.logo}>
                 <img src={logoImg.src} className={styles.logoImage}></img>
             </div>
-            <input className={styles.search}></input>
+            <div className={styles.searchContainer}>
+                <input className={styles.search} placeholder='Search movies'></input>
+                <div className={styles.searchLogoContainer}>
+                    <img className={styles.searchLogo} src={searchIcon.src}></img>
+                </div>
+            </div>
         </header>
     );
 };
