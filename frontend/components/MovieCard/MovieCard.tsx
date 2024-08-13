670c6d8b8bd4f8e89915f6658d3a1aa61588d35b
Hector Quinones <quinonhector1@gmail.com>
2024-08-13 17:39:24 -0500

    addding loading screen when content has not yet loaded

--- frontend/components/MovieCard/MovieCard.tsx
+++ frontend/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import styles from './MovieCard.module.css';
 import Image from 'next/image';
-
+import loading from '../../public/assets/Rolling.gif';
 
 interface MovieData{
     backdrop_path : string,
@@ -18,15 +18,23 @@ interface MovieCardProps{
 
 
 const MovieCard: React.FC<MovieCardProps> = ({responseObject}) => {
+    const [isLoading, setIsLoading] = useState(true);
     // console.log("RESPONSE OBJECT", responseObject);
     if(responseObject.media_type === "tv"){
         return null
     }
+
+    const handleImageLoad = () =>{
+        console.log("IMAGE LOADED");
+        setIsLoading(false);
+    };
+
     return (
         <div className={styles.movieCardContainer}>
-            <Image src={`https://image.tmdb.org/t/p/original/${responseObject.poster_path || responseObject.backdrop_path}`} alt={''} width={170} height={250} className={styles.movieImage}></Image>
-            <h1 className={styles.textStyle}>{responseObject.original_title || responseObject.original_name}</h1>
-            <h2 className={styles.textStyle2}>{responseObject.release_date}</h2>
+            {isLoading && (<Image src={loading.src} width={50} height={50} className={styles.loadingScreen} alt=''></Image>)}
+            <Image src={`https://image.tmdb.org/t/p/original/${responseObject.poster_path || responseObject.backdrop_path}`} alt={""} width={170} height={250} className={styles.movieImage} onLoad={handleImageLoad} onError={() => setIsLoading(true)}></Image>
+            {!isLoading && <h1 className={styles.textStyle}>{responseObject.original_title || responseObject.original_name}</h1>}
+            {!isLoading && <h2 className={styles.textStyle2}>{responseObject.release_date}</h2>}
         </div>
     );
 };
